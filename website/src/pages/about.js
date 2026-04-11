e3ca3b21e5c2e4f66803451a313fad698713880a
Mrdawudtheengineer <Reyan3rd@outlook.com>
2026-04-11 12:11:24 +0300

    Add About page with translation support

---
+++ website/src/pages/about.js
@@ -0,0 +1,11 @@
+import { useTranslation } from "react-i18next";
+export default function About() {
+  const { t } = useTranslation();
+  return (
+    <div>
+      <h1>{t('about.title')}</h1>
+      <p>{t('about.description')}</p>
+      {/* etc */}
+    </div>
+  );
+}
