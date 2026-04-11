b0ba5175e0bbfb712f3780bac82d974307d38c45
Mrdawudtheengineer <Reyan3rd@outlook.com>
2026-04-11 12:07:56 +0300

    Initialize i18n for internationalization support

---
+++ website/src/i18n.js
@@ -0,0 +1,18 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import translationEN from "./locales/en.json";
+import translationAM from "./locales/am.json";
+
+const resources = {
+  en: { translation: translationEN },
+  am: { translation: translationAM },
+};
+
+i18n.use(initReactI18next).init({
+  resources,
+  lng: "en", // default
+  fallbackLng: "en",
+  interpolation: { escapeValue: false }
+});
+
+export default i18n;
