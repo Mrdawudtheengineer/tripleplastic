18da4ff5f4647a3b4cdabc0b888755cf66aaf8fb
Mrdawudtheengineer <Reyan3rd@outlook.com>
2026-04-11 12:10:14 +0300

    Initialize React Router with basic routes

---
+++ website/src/app.js
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
+
+function App() {
+  return (
+    <Router>
+      {/* ...navigation here */}
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/services" component={Services} />
+        <Route path="/contact" component={Contact} />
+      </Switch>
+    </Router>
+  );
+}
